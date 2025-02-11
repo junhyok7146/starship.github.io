d562e8a1563a02b98f45afc978a9007cb80b54cd
junhyeok <156041703+junhyok7146@users.noreply.github.com>
2025-02-11 23:13:51 +0900

    api 추가 설정

---
+++ assets/firebase.js
@@ -0,0 +1,33 @@
+import firebase from 'firebase/compat/app'
+import { getAuth, signInWithPopup, GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/storage';
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyBErHWR3xI0igta2laDyKKwIitHUu75L5c",
+    authDomain: "kim-project-81d2d.firebaseapp.com",
+    databaseURL: "https://kim-project-81d2d-default-rtdb.firebaseio.com",
+    projectId: "kim-project-81d2d",
+    storageBucket: "kim-project-81d2d.appspot.com",
+    messagingSenderId: "589809226853",
+    appId: "1:589809226853:web:bf1c0a025356bf05b6e28c"
+  });
+
+const oDB = firebaseConfig.database()
+const auth = getAuth(firebaseConfig)
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+export { auth, githubProvider, googleProvider, signInWithPopup };
+
+
+export const productDB = oDB.ref('products')
+export const cartDB = oDB.ref('carts')
+export const orderDB = oDB.ref('orders')
+export const memberDB = oDB.ref('members')
+export const noticeDB = oDB.ref('notice')
+export const reviewDB = oDB.ref('review')
+
+export const oStorage = firebaseConfig.storage();
+
+
+
